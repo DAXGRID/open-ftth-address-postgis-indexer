73860883a0d7c2f17a4053653f343040f05864ec
Rune Nielsen <runenielsen@runbox.com>
2022-08-10 18:43:35 +0200

    add postgis connectionstring to settings

--- src/OpenFTTH.AddressPostgisProjector/Setting.cs
+++ src/OpenFTTH.AddressPostgisProjector/Setting.cs
@@ -7,8 +7,11 @@ internal sealed record Setting
     [JsonPropertyName("eventStoreConnectionString")]
     public string EventStoreConnectionString { get; init; }
 
+    [JsonPropertyName("postgisConnectionString")]
+    public string PostgisConnectionString { get; init; }
+
     [JsonConstructor]
-    public Setting(string eventStoreConnectionString)
+    public Setting(string eventStoreConnectionString, string postgisConnectionString)
     {
         if (String.IsNullOrWhiteSpace(eventStoreConnectionString))
         {
@@ -16,6 +19,13 @@ internal sealed record Setting
                 "Cannot be null or whitespace.", nameof(eventStoreConnectionString));
         }
 
+        if (String.IsNullOrWhiteSpace(postgisConnectionString))
+        {
+            throw new ArgumentException(
+                "Cannot be null or whitespace.", nameof(eventStoreConnectionString));
+        }
+
         EventStoreConnectionString = eventStoreConnectionString;
+        PostgisConnectionString = postgisConnectionString;
     }
 }
