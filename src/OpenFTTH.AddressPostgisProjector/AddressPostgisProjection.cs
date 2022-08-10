49cdb5756e614961fdc9168f8b0fec5b2964cc2c
Rune Nielsen <runenielsen@runbox.com>
2022-08-10 12:19:55 +0200

    now creates projection

---
+++ src/OpenFTTH.AddressPostgisProjector/AddressPostgisProjection.cs
@@ -0,0 +1,159 @@
+using Microsoft.Extensions.Logging;
+using OpenFTTH.Core.Address.Events;
+using OpenFTTH.EventSourcing;
+
+namespace OpenFTTH.AddressPostgisProjector;
+
+internal sealed record PostCode(string Code, string Name);
+
+internal sealed record AccessAddress(
+    Guid Id,
+    Guid RoadId,
+    string? TownName,
+    string HouseNumber,
+    Guid PostCodeId,
+    double NorthCoordinate,
+    double EastCoordinate);
+
+internal sealed class AddressPostgisProjection : ProjectionBase
+{
+    private uint _count;
+    private ILogger<AddressPostgisProjection> _logger;
+
+    public readonly Dictionary<Guid, PostCode> IdToPostCode = new();
+    public readonly Dictionary<Guid, string> IdToRoadName = new();
+    public readonly Dictionary<Guid, AccessAddress> IdToAddress = new();
+
+    public AddressPostgisProjection(ILogger<AddressPostgisProjection> logger)
+    {
+        _logger = logger;
+
+        ProjectEventAsync<PostCodeCreated>(ProjectAsync);
+        ProjectEventAsync<PostCodeUpdated>(ProjectAsync);
+        ProjectEventAsync<PostCodeDeleted>(ProjectAsync);
+
+        ProjectEventAsync<RoadCreated>(ProjectAsync);
+        ProjectEventAsync<RoadUpdated>(ProjectAsync);
+        ProjectEventAsync<RoadDeleted>(ProjectAsync);
+
+        ProjectEventAsync<AccessAddressCreated>(ProjectAsync);
+        ProjectEventAsync<AccessAddressUpdated>(ProjectAsync);
+        ProjectEventAsync<AccessAddressDeleted>(ProjectAsync);
+    }
+
+    private Task ProjectAsync(IEventEnvelope eventEnvelope)
+    {
+        _count++;
+
+        switch (eventEnvelope.Data)
+        {
+            case (PostCodeCreated postCodeCreated):
+                HandlePostCodeCreated(postCodeCreated);
+                break;
+            case (PostCodeUpdated postCodeUpdated):
+                HandlePostCodeUpdated(postCodeUpdated);
+                break;
+            case (PostCodeDeleted postCodeDeleted):
+                HandlePostCodeDeleted(postCodeDeleted);
+                break;
+            case (RoadCreated roadCreated):
+                HandleRoadCreated(roadCreated);
+                break;
+            case (RoadUpdated roadUpdated):
+                HandleRoadUpdated(roadUpdated);
+                break;
+            case (RoadDeleted roadDeleted):
+                HandleRoadDeleted(roadDeleted);
+                break;
+            case (AccessAddressCreated accessAddressCreated):
+                HandleAccessAddressCreated(accessAddressCreated);
+                break;
+            case (AccessAddressUpdated accessAddressUpdated):
+                HandleAccessAddressUpdated(accessAddressUpdated);
+                break;
+            case (AccessAddressDeleted accessAddressDeleted):
+                HandleAccessAddressDeleted(accessAddressDeleted);
+                break;
+            default:
+                throw new ArgumentException(
+                    $"Could not handle typeof '{eventEnvelope.Data.GetType().Name}'");
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private void HandleAccessAddressCreated(AccessAddressCreated accessAddressCreated)
+    {
+        IdToAddress.Add(
+            accessAddressCreated.Id,
+            new(
+                Id: accessAddressCreated.Id,
+                RoadId: accessAddressCreated.RoadId,
+                HouseNumber: accessAddressCreated.HouseNumber,
+                TownName: accessAddressCreated.TownName,
+                PostCodeId: accessAddressCreated.PostCodeId,
+                NorthCoordinate: accessAddressCreated.NorthCoordinate,
+                EastCoordinate: accessAddressCreated.EastCoordinate
+            ));
+    }
+
+    private void HandleAccessAddressUpdated(AccessAddressUpdated accessAddressUpdated)
+    {
+        var oldAccessAddress = IdToAddress[accessAddressUpdated.Id];
+        IdToAddress[accessAddressUpdated.Id] = oldAccessAddress with
+        {
+            RoadId = accessAddressUpdated.RoadId,
+            TownName = accessAddressUpdated.TownName,
+            PostCodeId = accessAddressUpdated.PostCodeId,
+            NorthCoordinate = accessAddressUpdated.NorthCoordinate,
+            EastCoordinate = accessAddressUpdated.EastCoordinate
+        };
+    }
+
+    private void HandleAccessAddressDeleted(AccessAddressDeleted accessAddressDeleted)
+    {
+        IdToAddress.Remove(accessAddressDeleted.Id);
+    }
+
+    private void HandlePostCodeCreated(PostCodeCreated postCodeCreated)
+    {
+        IdToPostCode.Add(
+            postCodeCreated.Id,
+            new(postCodeCreated.Number, postCodeCreated.Name));
+    }
+
+    private void HandlePostCodeUpdated(PostCodeUpdated postCodeUpdated)
+    {
+        var postCode = IdToPostCode[postCodeUpdated.Id];
+        IdToPostCode[postCodeUpdated.Id] = postCode with
+        {
+            Name = postCodeUpdated.Name
+        };
+    }
+
+    private void HandlePostCodeDeleted(PostCodeDeleted postCodeDeleted)
+    {
+        IdToPostCode.Remove(postCodeDeleted.Id);
+    }
+
+    private void HandleRoadCreated(RoadCreated roadCreated)
+    {
+        IdToRoadName.Add(roadCreated.Id, roadCreated.Name);
+    }
+
+    private void HandleRoadUpdated(RoadUpdated roadUpdated)
+    {
+        IdToRoadName[roadUpdated.Id] = roadUpdated.Name;
+    }
+
+    private void HandleRoadDeleted(RoadDeleted roadDeleted)
+    {
+        IdToRoadName.Remove(roadDeleted.Id);
+    }
+
+    public override Task DehydrationFinishAsync()
+    {
+        _logger.LogInformation("Finished dehydration a total of {Count} events.", _count);
+        return Task.CompletedTask;
+    }
+}
--- src/OpenFTTH.AddressPostgisProjector/AddressPostgisProjectorHost.cs
+++ src/OpenFTTH.AddressPostgisProjector/AddressPostgisProjectorHost.cs
@@ -1,20 +1,63 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OpenFTTH.EventSourcing;
+using System.Diagnostics;
 
 namespace OpenFTTH.AddressPostgisProjector;
 
 internal sealed class AddressPostgisProjectorHost : BackgroundService
 {
     private readonly ILogger<AddressPostgisProjectorHost> _logger;
+    private readonly IEventStore _eventStore;
+    private const int _catchUpTimeMs = 60000;
 
-    public AddressPostgisProjectorHost(ILogger<AddressPostgisProjectorHost> logger)
+    public AddressPostgisProjectorHost(
+        ILogger<AddressPostgisProjectorHost> logger,
+        IEventStore eventStore)
     {
         _logger = logger;
+        _eventStore = eventStore;
     }
 
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _logger.LogInformation("Starting {HostName}", nameof(AddressPostgisProjectorHost));
-        return Task.CompletedTask;
+        try
+        {
+            _logger.LogInformation(
+                "Starting {HostName}", nameof(AddressPostgisProjectorHost));
+
+            _logger.LogInformation("Starting dehydration.");
+            await _eventStore
+                .DehydrateProjectionsAsync(stoppingToken)
+                .ConfigureAwait(false);
+
+            _logger.LogInformation(
+                "Memory after dehydration {MibiBytes}.",
+                Process.GetCurrentProcess().PrivateMemorySize64 / 1024 / 1024);
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(_catchUpTimeMs, stoppingToken).ConfigureAwait(false);
+
+                _logger.LogInformation("Checking for new events.");
+                var changes = await _eventStore
+                    .CatchUpAsync(stoppingToken)
+                    .ConfigureAwait(false);
+
+                if (changes > 0)
+                {
+                    _logger.LogInformation("{Count} changes so we do import.", changes);
+                }
+                else
+                {
+                    _logger.LogInformation("No changes since last run.");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("{Exception}", ex);
+            throw;
+        }
     }
 }
